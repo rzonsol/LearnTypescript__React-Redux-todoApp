bacb838b3d40513b38bd4750e5399022dd38892f
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-29 14:24:34 +0200

    Loading info

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -9,8 +9,24 @@ interface AppProps {
 	deleteTodo: typeof deleteTodo;
 }
 
-class _App extends Component<AppProps> {
+interface AppState {
+	fetching: boolean;
+}
+
+class _App extends Component<AppProps, AppState> {
+	constructor(props: AppProps) {
+		super(props);
+		this.state = { fetching: false };
+	}
+
+	componentDidUpdate(prevPops: AppProps): void {
+		if (!prevPops.todos.length && this.props.todos.length) {
+			this.setState({ fetching: false });
+		}
+	}
+
 	onButtonClick = (): void => {
+		this.setState({ fetching: true });
 		this.props.fetchTodos();
 	};
 
@@ -29,10 +45,10 @@ class _App extends Component<AppProps> {
 	}
 
 	render() {
-		console.table(this.props.todos);
 		return (
 			<div>
 				<button onClick={this.onButtonClick}>Fetch</button>
+				{this.state.fetching ? 'LOADING' : null}
 				{this.renderList()}
 			</div>
 		);
